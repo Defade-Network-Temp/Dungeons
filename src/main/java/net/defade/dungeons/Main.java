4ec8f283c65b6111046776bb5b6784c777fab9ed
Am Gone <hygon806@gmail.com>
2022-11-12 18:04:37 +0100

    Initial Commit

---
+++ src/main/java/net/defade/dungeons/Main.java
@@ -0,0 +1,22 @@
+package net.defade.dungeons;
+
+import net.defade.bismuth.core.servers.GameType;
+import net.minestom.server.extensions.Extension;
+import org.jetbrains.annotations.Nullable;
+
+public class Main extends Extension {
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void terminate() {
+
+    }
+
+    @Override
+    public @Nullable GameType serverGameType() {
+        return new GameType("dungeons", "classic");
+    }
+}
