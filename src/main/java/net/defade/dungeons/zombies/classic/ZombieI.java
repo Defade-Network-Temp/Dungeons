63e60c3e035c43dfc7c94a39b2a6cadab650ba68
Am Gone <hygon806@gmail.com>
2022-11-20 21:55:07 +0100

    Add a pathfinder goal for the zombies

--- src/main/java/net/defade/dungeons/zombies/classic/ZombieI.java
+++ src/main/java/net/defade/dungeons/zombies/classic/ZombieI.java
@@ -1,11 +1,20 @@
 package net.defade.dungeons.zombies.classic;
 
 import net.defade.dungeons.zombies.DungeonsEntity;
+import net.defade.dungeons.zombies.ai.ClassicZombieGoal;
 import net.minestom.server.entity.EntityType;
+import net.minestom.server.entity.ai.EntityAIGroupBuilder;
+import java.time.Duration;
 
 public class ZombieI extends DungeonsEntity {
 
     public ZombieI() {
-        super(EntityType.ZOMBIE, "Lvl 1", 20, 0, 0, 5, 1.2f, 0, 2);
+        super(EntityType.ZOMBIE, "Lvl 1", 20, 0, 0, 5, 0.15, 0, 2);
+
+        addAIGroup(
+                new EntityAIGroupBuilder()
+                        .addGoalSelector(new ClassicZombieGoal(this, 3, Duration.ofMillis(1500)))
+                        .build()
+        );
     }
 }
