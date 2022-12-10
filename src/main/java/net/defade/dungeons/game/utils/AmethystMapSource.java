dc6680378c47ce39343f9f62f49e7f7f0f950442
Am Gone <hygon806@gmail.com>
2022-12-10 18:19:24 +0100

    Add a cache for maps

--- src/main/java/net/defade/dungeons/game/utils/AmethystMapSource.java
+++ src/main/java/net/defade/dungeons/game/utils/AmethystMapSource.java
@@ -9,14 +9,23 @@ import com.mongodb.client.model.Filters;
 import net.defade.yokura.amethyst.AmethystSource;
 import net.minestom.server.MinecraftServer;
 import org.bson.Document;
-import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
+import java.util.HexFormat;
 import java.util.concurrent.CompletableFuture;
 
 public class AmethystMapSource implements AmethystSource {
+    private static final Path TEMP_DIR = Path.of(System.getProperty("java.io.tmpdir") + File.separator + "defade");
+
     @Override
     public InputStream getSource() {
         GridFSBucket bucket = GridFSBuckets.create(MinecraftServer.getMongoDatabase(), "maps");
@@ -32,10 +41,37 @@ public class AmethystMapSource implements AmethystSource {
             throw new NullPointerException("Couldn't find a map.");
         }
 
-        try (GridFSDownloadStream downloadStream = bucket.openDownloadStream(mapDocument.get("filename", String.class), new GridFSDownloadOptions().revision(0))) {
-            return new ByteArrayInputStream(downloadStream.readAllBytes());
+        String fileName = mapDocument.getString("filename");
+        Path file = Path.of(TEMP_DIR + File.separator + fileName);
+
+        try {
+            if (!Files.exists(TEMP_DIR)) {
+                Files.createDirectory(TEMP_DIR);
+            } else if (Files.exists(file)) {
+                if (getFileMd5(file.toFile()).equals(mapDocument.getString("md5"))) {
+                    System.out.println("cache");
+                    return new FileInputStream(file.toFile());
+                }
+            }
+
+            Files.delete(file); // In case the file has changed and the md5 doesn't match anymore
+            Files.createFile(file);
+            FileOutputStream fileOutputStream = new FileOutputStream(file.toFile());
+            GridFSDownloadStream downloadStream = bucket.openDownloadStream(fileName, new GridFSDownloadOptions().revision(0));
+
+            byte[] buffer = new byte[256 * 1024]; // 256ko
+            int read;
+            while ((read = downloadStream.read(buffer)) != -1) {
+                fileOutputStream.write(buffer, 0, read);
+            }
+
+            downloadStream.close();
+            fileOutputStream.close();
+
+            System.out.println("database");
+            return new FileInputStream(file.toFile());
         } catch (IOException exception) {
-            exception.printStackTrace();
+            exception.printStackTrace(); // TODO Use logger and be more descriptive
             return null;
         }
     }
@@ -43,5 +79,25 @@ public class AmethystMapSource implements AmethystSource {
     @Override
     public OutputStream getOutputStream(CompletableFuture<Void> writeFuture) {
         return null; // Write is disabled.
+    }
+
+    private static String getFileMd5(File file) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+            FileInputStream fileInputStream = new FileInputStream(file);
+            byte[] buffer = new byte[256 * 1024]; // 256 ko
+
+            int read;
+            while ((read = fileInputStream.read(buffer)) != -1) {
+                messageDigest.update(buffer, 0, read);
+            }
+
+            fileInputStream.close();
+            return HexFormat.of().formatHex(messageDigest.digest());
+        } catch (NoSuchAlgorithmException | IOException exception) {
+            exception.printStackTrace(); // Should never happen
+        }
+
+        return "";
     }
 }
